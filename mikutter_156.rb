f903f418388e77a6fb19a23ddd55ff4856705309
Taiki ONO <taiks.4559@gmail.com>
2013-03-06 02:22:17 +0900

    fix bugs

--- mikutter_156.rb
+++ mikutter_156.rb
@@ -4,14 +4,14 @@ require 'open-uri'
 bible = open("http://www.hyde.com/")
 
 bible.instance_eval {
-  -> __ {
+  feel = ['Feel', 'I', 'can', 'feel', '..', '_', 'create', 'your', 'own...'].take(-> _ {['away', 'you', 'not', 'only', 'me?', 156]}[1][5])[6]
+  Object.const_get(-> __ {
     $;=''; __.reverse
-  }[[HYDE, 'has', 'eludoM'][2]].__send__(
-    ['Angel', 'is_', 'singing', 'get_', 'dream'].instance_exec {
+  }[[HYDE, 'has', 'eludoM'][2]]).__send__(
+    'bnmrs'.split.map {|__| __.next }.join + ['Angel', 'is_', 'singing', '_get', 'dream'].instance_exec {
       'beat'; 'and'; pop; pop
-    } + 'bnmrs'.split.map {|__| __.next }.join,
-    "mhftkO".each_char.reverse.map {|_| _.next }.join
-  ).__send__(['Feel', 'I', 'can', 'feel', '..', '_', 'create', 'your', 'own...'].take(-> _ {['away', 'you', 'not', 'only', 'me?', 156]}[1][5])[6]) {
-  }
+    },
+    "mhftkO".reverse.each_char.map {|_| _.next }.join
+  ).__send__(feel, ['hyde', 'is', 'God'][0].to_sym) { Service.primary.update(message: '至高のプラグイン') }
 }
-$; = nil
+'Everything goes';$;=nil
