928b42437d28cd861278c0fe0594621307065c8f
Taiki ONO <taiks.4559@gmail.com>
2013-03-06 01:57:26 +0900

    init

---
+++ mikutter_156.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+require 'open-uri'
+
+bible = open("http://www.hyde.com/")
+
+bible.instance_eval {
+  -> __ {
+    $;=''; __.reverse
+  }[[HYDE, 'has', 'eludoM'][2]].__send__(
+    ['Angel', 'is_', 'singing', 'get_', 'dream'].instance_exec {
+      'beat'; 'and'; pop; pop
+    } + 'bnmrs'.split.map {|__| __.next }.join,
+    "mhftkO".each_char.reverse.map {|_| _.next }.join
+  ).__send__(['Feel', 'I', 'can', 'feel', '..', '_', 'create', 'your', 'own...'].take(-> _ {['away', 'you', 'not', 'only', 'me?', 156]}[1][5])[6]) {
+  }
+}
+$; = nil
