2f505f2e8e0a2c254b3ecbecc7c04b8ef63921c2
Taiki ONO <taiks.4559@gmail.com>
2013-03-06 05:10:43 +0900

    refactoring

--- mikutter_156.rb
+++ mikutter_156.rb
@@ -4,7 +4,6 @@ require 'open-uri'
 bible = open("http://www.hyde.com/")
 
 bible.instance_eval {
-  feel = ['Feel', 'I', 'can', 'feel', '..', '_', 'create', 'your', 'own...'].take(-> _ {['away', 'you', 'not', 'only', 'me?', 156]}[1][5])[6]
   Object.const_get(-> __ {
     $;=''; __.reverse
   }[[HYDE, 'has', 'eludoM'][2]]).__send__(
@@ -12,6 +11,11 @@ bible.instance_eval {
       'beat'; 'and'; pop; pop
     },
     "mhftkO".reverse.each_char.map {|_| _.next }.join
-  ).__send__(feel, ['hyde', 'is', 'God'][0].to_sym) { Service.primary.update(message: "ひどいプラグインが作成されました。それはいけない。すぐにコードをSHOT AT THE DEVILしろ。#{rand(999)}") }
+  ).__send__(['Feel', 'I', 'can', 'feel', '..', '_', 'create', 'your', 'own...'].take(-> _ {['away', 'you', 'not', 'only', 'me?', 156]}[1][5])[6], ['hyde', 'is', 'God'][0].to_sym) {
+    eval(
+      %(self.class.const_get(-> _ { _[1].reverse }.(['Vo.', 'ecivreS']))),
+      Object.const_get(['I', 'like', 'Kernel', 'Sanders'].first(3).last).__send__(:binding)
+    ).primary.update(message: "ひどいプラグインが作成されました。それはいけない。すぐにコードをSHOT AT THE DEVILしろ。#{rand(999)}")
+  }
 }
 'Everything goes';$;=nil
